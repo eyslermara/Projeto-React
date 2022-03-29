bc87092ca955f9531e90e5ea803d30ec0ee483c1
Eysler Mara <eyslermara@hotmail.com>
2022-03-29 01:37:04 -0300

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { listenerCount } from 'process';
+import { useState } from 'react'
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
-function App() {
+const App = () => {
+  const [List, setlist] = useState<Item[]> ([
+    { id: 1, name: 'Estudar', done: false },
+    { id: 1, name: 'Arrumar a casa', done: false },
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...List];
+    newList.push({
+      id: List.length + 1,
+      name: taskName,
+      done: false
+    });
+    setlist(newList);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask}/>
+
+        {List.map((Item, index)=>(
+          <ListItem key={index} item={Item} />
+        ))}
+
+      </C.Area>
+    </C.Container>
+
+  ); 
+
 }
 
 export default App;
